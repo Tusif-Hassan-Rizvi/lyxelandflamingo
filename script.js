1ad97ea2e2bfefe2b84bb909a056972632e7179a
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2023-09-13 18:49:16 +0530

    added reponsive design at navbar

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ $('.slick-carousel').slick({
   arrows: true, // Adds arrows to sides of slider
   dots: false, // Adds the dots on the bottom
 });
+
+// Function to update the window dimensions
+function updateWindowDimensions() {
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
+
+  // Display the updated width and height
+  console.log("Window Width: " + windowWidth);
+  console.log("Window Height: " + windowHeight);
+
+  // You can also perform additional actions based on the new dimensions here
+}
+
+// Add an event listener for the 'resize' event
+window.addEventListener('resize', updateWindowDimensions);
+
+// Call the function once to get the initial dimensions
+updateWindowDimensions();
